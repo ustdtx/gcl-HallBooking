a19dd84c2a44c26f92c4d6f02bf1ec4a9a089c7c
ustdtx <tamim18016@gmail.com>
2026-01-16 14:15:07 +0600

    prep for refacotring

--- frontend/src/components/Gapbg.jsx
+++
@@ -1,6 +0,0 @@
-export default function Gapbg() {
-  return (
-    <div className="fixed top-116 left-0 right-0 w-full h-full bg-[#232323] py-2 px-4 flex items-center shadow-md z-[-2]">
-    </div>
-  );
-}
